42b1681988fac66d56369d1593beb3a5ac1428f4
yutoVR <yuto.fushimi@icloud.com>
2020-03-13 13:37:40 +0900

    Create Unity Project

---
+++ Assets/Scripts/LoadCSVTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadCSVTest : MonoBehaviour
+{
+	[SerializeField] Text uiText;
+	[SerializeField] TextAsset textAsset;
+	[SerializeField] CSVParser.Delimiter delimiter;
+
+	void Load()
+	{
+		var sheet = CSVParser.LoadFromString(textAsset.text);
+
+		var log = "";
+		foreach (var row in sheet)
+		{
+			log += "|";
+
+			foreach (var cell in row)
+			{
+				log += cell + "|";
+			}
+
+			log += "\n";
+		}
+
+		Debug.Log(log);         // Unity
+		Console.WriteLine(log); // C#
+		uiText.text = log;
+	}
+
+	void OnValidate()
+	{
+		Load();
+	}
+}
