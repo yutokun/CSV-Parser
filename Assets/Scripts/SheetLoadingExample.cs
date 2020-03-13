a5ab1ac99301d2e167cfb004e76af9f3802c6c4a
yutoVR <yuto.fushimi@icloud.com>
2020-03-13 13:43:30 +0900

    Better script name

--- Assets/Scripts/SheetLoadingExample.cs
+++ Assets/Scripts/SheetLoadingExample.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LoadCSVTest : MonoBehaviour
+public class SheetLoadingExample : MonoBehaviour
 {
 	[SerializeField] Text uiText;
 	[SerializeField] TextAsset textAsset;
