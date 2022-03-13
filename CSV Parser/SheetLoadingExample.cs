d7f14fd746d4f2c2a623e42a13194c4c4b700adb
yutoVR <yuto.fushimi@icloud.com>
2022-03-13 15:08:28 +0900

    Removed Unity-related objects.

---
+++ CSV Parser/SheetLoadingExample.cs
@@ -0,0 +1,20 @@
+﻿public class SheetLoadingExample
+{
+    void Load()
+    {
+        var sheet = CSVParser.LoadFromString("textAsset.text", CSVParser.Delimiter.Tab);
+
+        var log = "";
+        foreach (var row in sheet)
+        {
+            log += "|";
+
+            foreach (var cell in row)
+            {
+                log += cell + "|";
+            }
+
+            log += "\n";
+        }
+    }
+}
