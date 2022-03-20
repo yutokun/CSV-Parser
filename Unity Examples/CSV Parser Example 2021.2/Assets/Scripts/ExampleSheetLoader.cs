0fc60fb5d910fb8864b94d0057741ff3d9262311
yutoVR <yuto.fushimi@icloud.com>
2022-03-21 01:59:56 +0900

    Re-add scripts.

---
+++ Unity Examples/CSV Parser Example 2021.2/Assets/Scripts/ExampleSheetLoader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace yutokun
+{
+    public class ExampleSheetLoader : MonoBehaviour
+    {
+        [SerializeField]
+        Text result;
+
+        [SerializeField]
+        TextAsset[] examples;
+
+        int index = -1;
+
+        void Start()
+        {
+            ShowNextSheet();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ShowNextSheet();
+            }
+        }
+
+        void ShowNextSheet()
+        {
+            index = (int)Mathf.Repeat(index + 1, examples.Length);
+            var sheet = CSVParser.LoadFromString(examples[index].text);
+            var styled = Stylize(sheet);
+
+            Debug.Log(styled);
+            result.text = styled;
+        }
+
+        static string Stylize(List<List<string>> sheet)
+        {
+            var styled = new StringBuilder();
+            foreach (var row in sheet)
+            {
+                styled.Append("| ");
+
+                foreach (var cell in row)
+                {
+                    styled.Append(cell);
+                    styled.Append(" | ");
+                }
+
+                styled.AppendLine();
+            }
+
+            return styled.ToString();
+        }
+    }
+}
